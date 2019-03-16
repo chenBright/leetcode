f4d2077706d2a93d111b51b4e2e131038d29e33a
chenBright <1021774709@qq.com>
2019-03-16 12:10:52 +0800

    Feature: 122-买卖股票的最佳时机 II (#38)

---
+++ 122-买卖股票的最佳时机 II/maxProfit_gerrdy1.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int profit = 0;
+        if (prices.empty()) {
+            return profit;
+        }
+        
+        int vally = 0;
+        int peak = 0;
+        auto it = prices.begin();
+        while (it + 1 != prices.end()) {
+            // 找到极小值
+            while (it + 1 != prices.end() && *it >= *(it + 1)) {
+                ++it;
+            }
+            vally = *it;
+
+            // 找到极大值
+            while (it + 1 != prices.end() && *it <= *(it + 1)) {
+                ++it;
+            }
+            peak = *it;
+
+            profit += peak - vally;
+        }
+
+        return profit;
+    }
+};
--- README.md
+++ README.md
@@ -33,3 +33,4 @@ LeetCode Problems' Solutions
 - [118-杨辉三角](https://leetcode-cn.com/problems/pascals-triangle/)
 - [119-杨辉三角 II](https://leetcode-cn.com/problems/pascals-triangle-ii/)
 - [121-买卖股票的最佳时机](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/)*（动态规划）
+- [122-买卖股票的最佳时机 II](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-ii/)*（贪心算法）
