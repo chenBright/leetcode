a8648af96c39a76c9fa04ace8c3ec7a0eb191eaf
chenBright <1021774709@qq.com>
2019-03-17 11:42:29 +0800

    Feature: 188-买卖股票的最佳时机 IV

---
+++ 188-买卖股票的最佳时机 IV/README.md
@@ -0,0 +1,7 @@
+# 188-买卖股票的最佳时机 IV
+
+## 题目
+
+leetcode：[188-买卖股票的最佳时机 IV](https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-iv/)
+
+## 迭代
---
+++ 188-买卖股票的最佳时机 IV/maxProfit.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int profit = 0;
+        if (prices.empty()) {
+            return profit;
+        }
+
+        for (auto it = prices.begin(); it + 1 != prices.end(); ++it) {
+            int diff = *(it + 1) - *it;
+            // 只要第i天的价格比第i+1天的价格低，则在第i天买入，第i+1天卖出
+            if (diff > 0) {
+                profit += diff;
+            }
+        }
+
+        return profit;
+    }
+};
