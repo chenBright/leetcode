baefc575e8cdcedfcfdb4d828433b63ed5c012c6
chenBright <1021774709@qq.com>
2019-05-08 12:01:49 +0800

    Feature：055-跳跃游戏 (#107)

---
+++ 055-跳跃游戏/canJump_greedy2.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool canJump(vector<int> &nums) {
+        int length = nums.size();
+        if (length == 0) {
+            return false;
+        }
+
+        int dist = 0;
+        // dist 表示能跳到最大的位置
+        // dist >= i，表示至少跳出本个位置，即 num[i] != 0
+        for (int i = 0; i < length && dist >= i; ++i) {
+            dist = max(dist, i + nums[i]);
+        }
+
+        return dist >= length - 1;
+    }
+};
