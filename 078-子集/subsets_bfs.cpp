0c2ab8f30babb1df74702ef348ced941001482a0
chenBright <1021774709@qq.com>
2019-05-12 11:49:20 +0800

    Feature：078-子集 (#114)

---
+++ 078-子集/subsets_bfs.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+using namespace std;
+
+// BFS 回溯法
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &nums) {
+        int length = nums.size();
+        if (length == 0) {
+            return res;
+        }
+        subsets(nums, 0, length - 1);
+
+        return res;
+    }
+
+private:
+    vector<vector<int> > res;
+    vector<int> temp;
+
+    void subsets(vector<int> &nums, int left, int right) {
+        if (left > right) {
+            res.push_back(temp);
+            return;
+        }
+
+        res.push_back(temp); // 先不考虑后面的元素，先添加当前集合
+        // 与后面没个元素分别组成一个集合
+        for (int i = left; i <= right; ++i) {
+            temp.push_back(nums[i]);
+            subsets(nums, i + 1, right);
+            temp.pop_back();
+        }
+    }
+};
