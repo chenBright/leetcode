ac9734dc719ccf556dedb5e3a85495ac0b573b53
chenBright <1021774709@qq.com>
2019-05-17 09:29:04 +0800

    Feature：105-从前序与中序遍历序列构造二叉树

---
+++ 105-从前序与中序遍历序列构造二叉树/README.md
@@ -0,0 +1,9 @@
+# 105-从前序与中序遍历序列构造二叉树
+
+## 题目
+
+leetcode：[105-从前序与中序遍历序列构造二叉树](https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/)
+
+
+## 递归
+
---
+++ 105-从前序与中序遍历序列构造二叉树/buildTree.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder) {
+        
+    }
+
+private:
+    void buildTreeRecursion() {
+        
+    }
+};
