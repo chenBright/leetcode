645892b287c8b9119e7c02792f5863617da6fd9f
chenBright <1021774709@qq.com>
2019-05-17 10:20:14 +0800

    Feature：105-从前序与中序遍历序列构造二叉树

--- 105-从前序与中序遍历序列构造二叉树/buildTree.cpp
+++ 105-从前序与中序遍历序列构造二叉树/buildTree.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 struct TreeNode {
@@ -11,11 +12,41 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder) {
-        
+        return buildTreeRecursion(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
     }
 
 private:
-    void buildTreeRecursion() {
-        
+    TreeNode* buildTreeRecursion(vector<int> &preorder, int preLow, int preHigh, 
+                            vector<int> &inorder, int inLow, int inHigh) {
+        if (preLow > preHigh || inLow > inHigh) {
+            return NULL;
+        }
+
+        if (preHigh - preLow != inHigh - inLow) {
+            return NULL;
+        }
+
+        if (preorder.empty()) {
+            return NULL;
+        }
+
+        int rootVal = preorder[preLow];
+        int rootIndex = -1;
+        for (int i = inLow; i <= inHigh; ++i) {
+            if (inorder[i] == rootVal) {
+                rootIndex = i;
+                break;
+            }
+        }
+
+        if (rootIndex < inLow || rootIndex > inHigh) {
+            return NULL;
+        }
+
+        TreeNode *root = new TreeNode(rootVal);
+        root->left = buildTreeRecursion(preorder, preLow + 1, preLow + (rootIndex - inLow), inorder, inLow, rootIndex - 1);
+        root->right = buildTreeRecursion(preorder, preLow + (rootIndex - inLow) + 1, preHigh, inorder, rootIndex + 1, inHigh);
+
+        return root;
     }
 };
