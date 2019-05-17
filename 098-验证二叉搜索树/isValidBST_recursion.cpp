ccb2af0343417fd2a9da71f2b83759a84d704e1b
chenBright <1021774709@qq.com>
2019-05-17 09:35:21 +0800

    Debug：修改文件名 (#124)

--- 098-验证二叉搜索树/isValidBST_recursion.cpp
+++ 098-验证二叉搜索树/isValidBST_recursion.cpp
@@ -1,6 +1,4 @@
-#include <stack>
-#include <climits>
-using namespace std;
+#include <iostream>
 
 struct TreeNode {
     int val;
@@ -12,25 +10,27 @@ struct TreeNode {
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        long lastValue = LONG_MIN; // 上一结点值。类型为 long，是为了避免 node->val = INT_MIN 出错的情况
-        stack<TreeNode*> nodeStack;
-        TreeNode *node = root;
-        while (!nodeStack.empty() || node != NULL) {
-            if (node != NULL) {
-                nodeStack.push(node);
-                node = node->left;
-            } else {
-                node = nodeStack.top();
-                nodeStack.pop();
-                if (node->val > lastValue) {
-                    lastValue = node->val;
-                } else {
-                    return false;
-                }
-                node = node->right;
-            }
+        lastValue = LONG_MIN;
+        return isValidBSTRecursion(root);
+    }
+
+private:
+    long lastValue; // 上一结点值。类型为 long，是为了避免 node->val = INT_MIN 出错的情况
+    bool isValidBSTRecursion(TreeNode *root) {
+        if (root == NULL) {
+            return true;
         }
 
-        return true;
+        bool leftResult = isValidBSTRecursion(root->left);
+
+        // 左子树不是二叉搜索树 或者 左结点值大于等于根结点值
+        if (!leftResult || lastValue >= root->val) {
+            return false;
+        }
+        lastValue = root->val;
+
+        bool rightResult = isValidBSTRecursion(root->right);
+
+        return rightResult;
     }
 };
