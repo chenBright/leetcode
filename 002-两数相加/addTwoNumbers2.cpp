b6fcc8236e0265e3ca4d16c504b92dd1c3369d31
chenBright <1021774709@qq.com>
2019-05-30 20:14:27 +0800

    Opt：增加一种简洁的实现 (#145)
    * Opt：增加一种简洁的实现
    
    * Opt：增加一种简洁的实现

---
+++ 002-两数相加/addTwoNumbers2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode *l1, ListNode *l2) {
+        if (l1 == NULL) {
+            return l2;
+        }
+        if (l2 == NULL) {
+            return l1;
+        }
+
+        ListNode *head = new ListNode(-1);
+        ListNode *node = head;
+        int carryFlag = 0;
+        while (l1 != NULL || l2 != NULL || carryFlag == 1) {
+            int sum = 0;
+            if (l1 != NULL) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2 != NULL) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            if (carryFlag == 1) {
+                sum += 1;
+            }
+
+            if (sum >= 10) {
+                sum -= 10;
+                carryFlag = 1;
+            } else {
+                carryFlag = 0;
+            }
+            node->next = new ListNode(sum);
+            node = node->next;
+        }
+
+        return head->next;
+    }
+};
