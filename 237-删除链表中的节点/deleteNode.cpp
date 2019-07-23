e9f281291cddb6c145678d4b111e0d502787eb66
chenBright <1021774709@qq.com>
2019-07-24 00:22:39 +0800

    Feature：237-删除链表中的节点

---
+++ 237-删除链表中的节点/deleteNode.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+ 
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode *next = node->next;
+        node->val = next->val;
+        node->next = next->next;
+    }
+};
