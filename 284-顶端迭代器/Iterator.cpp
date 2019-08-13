741547f26b63b3f59b2bfaee5030a7f67f5485e2
chenBright <1021774709@qq.com>
2019-08-13 23:24:32 +0800

    Feature：284-顶端迭代器 (#238)

---
+++ 284-顶端迭代器/Iterator.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+using namespace std;
+
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+
+class Iterator {
+    struct Data;
+	Data* data;
+public:
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+public:
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+		// 派生类向基类转换类型
+        return Iterator(*this).next();
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    return Iterator::next();
+	}
+
+	bool hasNext() const {
+	    return Iterator::hasNext();
+	}
+};
