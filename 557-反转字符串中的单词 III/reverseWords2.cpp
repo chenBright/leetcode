babcd3c25c15171d5a3614c798365d04b94e5dcf
chenBright <1021774709@qq.com>
2019-10-06 00:18:00 +0800

    Feature: 557-反转字符串中的单词 III (#290)

---
+++ 557-反转字符串中的单词 III/reverseWords2.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int length = s.size();
+        for (int i = 0; i < length;) {
+            int j = i + 1;
+            while (j < length && s[j] != ' ') {
+                ++j;
+            }
+
+            reverseWords(s, i, j - 1);
+
+            i = j + 1;
+            // 跳过空格
+            while (i < length && s[i] == ' ') {
+                ++i;
+            }
+        }
+
+        return s;
+    }
+
+private:
+    void reverseWords(string& s, int start, int end) {
+        while (start < end) {
+            s[start] += s[end];
+            s[end] = s[start] - s[end];
+            s[start] = s[start] - s[end];
+            ++start;
+            --end;
+        }
+    }
+};
