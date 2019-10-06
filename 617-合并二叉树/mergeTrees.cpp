1221c0d78fe89d9a8beb1520974f6f25ab0ce40d
chenBright <1021774709@qq.com>
2019-10-06 11:33:08 +0800

    Feature: 617-合并二叉树

---
+++ 617-合并二叉树/mergeTrees.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+ 
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (t1 == NULL && t2 == NULL) {
+            return NULL;
+        } else if (t1 == NULL && t2 != NULL) {
+            return t2;
+        } else if (t1 != NULL && t2 == NULL) {
+            return t1;
+        } else {
+            t1->val += t2->val;
+            t1->left = mergeTrees(t1->left, t2->left);
+            t1->right = mergeTrees(t1->right, t2->right);
+
+            return t1;
+        }
+    }
+};
