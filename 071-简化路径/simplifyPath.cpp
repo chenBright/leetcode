53faac923490eb13e3b5169a900822c749c0ab95
chenBright <1021774709@qq.com>
2019-12-18 13:37:40 +0800

    Opt: 优化字符串分割方法

--- 071-简化路径/simplifyPath.cpp
+++ 071-简化路径/simplifyPath.cpp
@@ -1,56 +1,33 @@
 #include <string>
 #include <stack>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 class Solution {
 public:
     string simplifyPath(string path) {
-        int length = path.size();
-        if (length == 0) {
-            return "/";
-        }
-
         vector<string> dirs;
-        int i = 0;
-        // 分割字符串，分割字符是“/”
-        while (i < length) {
-            if (path[i] == '/') {
-                ++i;
-                continue;
-            } else {
-                string str(1, path[i]);
-                int j = i + 1;
-                while (j < length && path[j] != '/') {
-                    str += path[j];
-                    ++j;
-                }
-                dirs.push_back(str);
-                i = j;
+        split(path, dirs, '/');
+
+        string result = "";
+        for (const auto& d : dirs) {
+            result += "/" + d;
+        }
+
+        return result.empty() ? "/" : result;
+    }
+
+private:
+    void split(const std::string& str, std::vector<std::string>& tokens, const char delim = ' ') {
+        std::istringstream iss(str);
+        std::string tmp;
+        while (std::getline(iss, tmp, delim)) {
+            if (!tokens.empty() && tmp == "..") {
+                tokens.pop_back();
+            } else if (!tmp.empty() && tmp != "." && tmp != "..") {
+                tokens.emplace_back(std::move(tmp));
             }
         }
-
-        // 处理文件夹
-        stack<string> readDirs;
-        for (const auto &dir : dirs) {
-            if (dir == ".") {
-                continue;
-            } else if (dir != "..") {
-                readDirs.push(dir);
-            } else {
-                if (!readDirs.empty()) {
-                    readDirs.pop();
-                }
-            }
-        }
-
-        // 拼接路径字符串
-        string res = "";
-        while (!readDirs.empty()) {
-            res = "/" + readDirs.top() + res;
-            readDirs.pop();            
-        }
-
-        return res.empty() ? "/" : res;
     }
 };
