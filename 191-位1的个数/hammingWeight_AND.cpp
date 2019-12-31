8b54d8bed366530bddbf35636b559a74ab9c2d8e
chenBright <1021774709@qq.com>
2019-12-31 10:11:05 +0800

    Opt: 优化代码

--- 191-位1的个数/hammingWeight_AND.cpp
+++ 191-位1的个数/hammingWeight_AND.cpp
@@ -4,7 +4,6 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
-        int i = 32;
         for (int i = 0; i < 32; ++i) {
             count += n & 1; // 获取n的最右边的位
             n >>= 1; // 删除最右边的位
