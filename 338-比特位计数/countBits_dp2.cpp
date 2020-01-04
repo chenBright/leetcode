55027fa864dc87423963678f48db6f9121fbd255
chenBright <1021774709@qq.com>
2020-01-04 10:25:08 +0800

    Opt: 补充注释

--- 338-比特位计数/countBits_dp2.cpp
+++ 338-比特位计数/countBits_dp2.cpp
@@ -8,6 +8,10 @@ public:
         int i = 0;
         int b = 1;
         for (int i = 1; i <= num; ++i) {
+            //  i >> 1 后比原来 i “在最右边少了一位”，
+            // 如果原来 i 的最低位是 1，则 result[i] = result[i >> 1] + 1；
+            // 如果原来 i 的最低位是 0，则 result[i] = result[i >> 1]。
+            // i & 1 或者 i % 2 可用于判断做低位是否为 1。
             // result[i] = result[i / 2] + (i % 2);
             result[i] = result[i >> 1] + (i & 1);
         }
