4390eaf02e98acd0decf9bd6b34bc16f882aaee4
chenBright <1021774709@qq.com>
2020-01-28 14:03:16 +0800

    Opt: 使用索引代替迭代器

--- 122-买卖股票的最佳时机 II/maxProfit_greedy2.cpp
+++ 122-买卖股票的最佳时机 II/maxProfit_greedy2.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int> &prices) {
+    int maxProfit(vector<int>& prices) {
         int profit = 0;
-        for (auto it = prices.begin(); it + 1 != prices.end(); ++it) {
-            int diff = *(it + 1) - *it;
-            // 只要第i天的价格比第i+1天的价格低，则在第i天买入，第i+1天卖出
+        for (int i = 0; i + 1 < prices.size(); ++i) {
+            int diff = prices[i + 1] - prices[i];
             if (diff > 0) {
                 profit += diff;
             }
