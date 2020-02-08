5bfb57d095f303552a0bef90f3df06aacf07806c
chenBright <1021774709@qq.com>
2020-02-08 19:05:33 +0800

    Opt: 优化代码和完善注释

--- 018-四数之和/fourSum.cpp
+++ 018-四数之和/fourSum.cpp
@@ -5,24 +5,30 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int> > fourSum(vector<int> &nums, int target) {
-        vector<vector<int> > res;
-        int len = nums.size();
+        vector<vector<int> > result;
+        int length = nums.size();
         sort(nums.begin(), nums.end());
-        for (int i = 0; i < len; ++i) {
+
+        // 不需要完全遍历数组，主需要遍历到 nums[length - 4]即可。
+        // 因为结果为三元组，nums[length - 3]、nums[length - 2]、nums[length - 1]
+        // 没有足够的元素与之组成四元组，所以不需要遍历它们。
+        for (int i = 0; i <= length - 4; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) { // 去重
                 continue;
             }
-            for (int j = i + 1; j < len; ++j) {
+            // 不需要完全遍历数组，主需要遍历到 nums[length - 3]即可。
+            // 因为结果为三元组，nums[length - 2]、nums[length - 1]
+            // 没有足够的元素与之组成三元组，所以不需要遍历它们。
+            for (int j = i + 1; j <= length - 3; ++j) {
                 if (j > i + 1 && nums[j] == nums[j - 1]) { // 去重
                     continue;
                 }
                 int m = j + 1;
-                int n =len - 1;
+                int n =length - 1;
                 while (m < n) {
                     int sum = nums[i] + nums[j] + nums[m] + nums[n];
                     if (sum == target) {
-                        vector<int> temp = {nums[i], nums[j], nums[m], nums[n]};
-                        res.push_back(temp);
+                        result.push_back({nums[i], nums[j], nums[m], nums[n]});
                         ++m;
                         --n;
                         while (m < n && nums[m] == nums[m - 1]) { // 去重
@@ -39,6 +45,6 @@ public:
                 }
             }
         }
-        return res;
+        return result;
     }
 };
