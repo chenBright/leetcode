098f17f7b14ced27527e6d5973d5b9508860b53f
chenBright <1021774709@qq.com>
2020-02-18 20:14:34 +0800

    Opt: 优化代码 (#384)

--- 448-找到所有数组中消失的数字/findDisappearedNumbers_abs.cpp
+++ 448-找到所有数组中消失的数字/findDisappearedNumbers_abs.cpp
@@ -5,14 +5,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            nums[abs(nums[i]) - 1] = -abs(nums[abs(nums[i]) - 1]);
+        int length = nums.size();
+        for (int i = 0; i < length; ++i) {
+            int num = abs(nums[i]);
+            if (nums[num - 1] > 0) {
+                nums[num - 1] *= -1;
+            }
         }
-        
+
         vector<int> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] > 0) {
-                result.push_back(i + 1);
+        for (int j = 0; j < length; ++j) {
+            if (nums[j] > 0) {
+                result.push_back(j + 1);
             }
         }
 
--- 448-找到所有数组中消失的数字/findDisappearedNumbers_bucket.cpp
+++ 448-找到所有数组中消失的数字/findDisappearedNumbers_bucket.cpp
@@ -5,18 +5,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
+        int length = nums.size();
+        for (int i = 0; i < length; ++i) {
             // 将 nums[i] 放到 nums[nums[i] - 1] 的位置上
             if (nums[i] != nums[nums[i] - 1]) {
                 swap(nums[i], nums[nums[i] - 1]);
-                --i;
             }
         }
 
         vector<int> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != i + 1) {
-                result.push_back(i + 1);
+        for (int j = 0; j < length; ++j) {
+            if (nums[j] != j + 1) {
+                result.push_back(j + 1);
             }
         }
 
