3375816bf38ec02cba8fd5fb73ea564fc7f1631d
chenBright <1021774709@qq.com>
2020-03-13 23:19:23 +0800

    Opt: 优化代码 (#425)

--- 042-接雨水/trap_col_dp2.cpp
+++ 042-接雨水/trap_col_dp2.cpp
@@ -16,9 +16,9 @@ public:
         for (int i = length - 2; i >= 0; --i) {
             maxRights[i] = max(maxRights[i + 1], height[i + 1]);
         }
-        
+
         int maxLeft = 0;
-        for (int j = 1; j < height.size() - 1; ++j) {
+        for (int j = 1; j < length - 1; ++j) {
             maxLeft = max(maxLeft, height[j - 1]);
             int minHeight = min(maxLeft, maxRights[j]);
             // 只有较小的一段大于当前列的高度才会有水，其他情况不会有水
