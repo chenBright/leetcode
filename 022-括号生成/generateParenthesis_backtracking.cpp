d12594cbd86dabb205890c83443fe89f5005978c
chenBright <1021774709@qq.com>
2020-04-03 21:00:10 +0800

    Opt: 优化代码 (#448)

--- 022-括号生成/generateParenthesis_backtracking.cpp
+++ 022-括号生成/generateParenthesis_backtracking.cpp
@@ -16,21 +16,21 @@ public:
     }
 
 private:
-    void generate(string str, int left, int right, int max, vector<string> &result) {
-        if (left + right == 2 * max) {
+    void generate(string str, int left, int right, int n, vector<string> &result) {
+        if (left + right == 2 * n) {
             result.push_back(str);
             return;
         }
 
-        // 只要左括号没够 max 个，都可以添加到左括号
-        if (left < max) {
-            generate(str + "(", left + 1, right, max, result);
+        // 只要左括号没够 n 个，都可以添加到左括号
+        if (left < n) {
+            generate(str + "(", left + 1, right, n, result);
         }
 
         // 只有左括号数大于右括号数，才可以添加到右括号；
         // 不然，会生成不合法字符，即右括号与左括号不匹配
         if (left > right) {
-            generate(str + ")", left, right + 1, max, result);
+            generate(str + ")", left, right + 1, n, result);
         }
 
     }
