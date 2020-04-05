6a8ef940e85ccdb2a1ff5c1512fe0f0006fe56dc
chenBright <1021774709@qq.com>
2020-04-05 21:45:15 +0800

    Feature: 876-链表的中间结点

---
+++ 876-链表的中间结点/middleNode.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+struct ListNode {
+int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
