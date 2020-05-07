8404cd8768024aaf1ae198707606177d0a564ade
chenBright <1021774709@qq.com>
2020-05-07 10:47:03 +0800

    Opt: 优化代码

--- 096-不同的二叉搜索树/numTrees_dp.cpp
+++ 096-不同的二叉搜索树/numTrees_dp.cpp
@@ -4,12 +4,12 @@ using namespace std;
 class Solution {
 public:
     int numTrees(int n) {
-        // dp[i]表示i个结点的二叉搜索树的个数
+        // dp[i] 表示 i 个结点的二叉搜索树的个数。
         vector<int> dp(n + 1, 0);
         dp[0] = 1;
         for (int i = 1; i <= n; ++i) {
             for (int j = 1; j <= i; ++j) {
-                // 结点j为根结点时，二叉搜索树的个数
+                // 结点 j 为根结点时，二叉搜索树的个数。
                 dp[i] += dp[j - 1] * dp[i - j];
             }
         }
