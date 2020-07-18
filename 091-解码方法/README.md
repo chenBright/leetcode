5fe97dd8ec1e44bd3a19ff812ef8aa6dada3a0c1
chenBright <1021774709@qq.com>
2020-07-18 11:21:12 +0800

    Opt: 记忆化搜索 (#472)
    * Opt: 优化代码
    
    * Opt: 记忆化搜索

--- 091-解码方法/README.md
+++ 091-解码方法/README.md
@@ -4,35 +4,45 @@
 
 leetcode：[091-解码方法](https://leetcode-cn.com/problems/decode-ways/)
 
-## 递归
-
-递归实现效率不高，在LeetCode上会"超出时间限制"。
+## 记忆化搜索
 
 ```c++
 class Solution {
 public:
     int numDecodings(string s) {
-        int length = s.size();
+        if (s.empty()) {
+            return 0;
+        }
+        unordered_map<int, int> m;
 
-        if (length == 0) {
+        return numDecodings(s, 0, m);
+    }
+private:
+    int numDecodings(string& s, int i, unordered_map<int, int>& m) {
+        if (m.count(i)) {
+            return m[i];
+        }
+
+        int length = s.size();
+        if (i == length) {
             return 1;
         }
-        // 第一个字符为'0'，不能解码
-        if (s[0] == '0') {
+
+        if (s[i] == '0') {
             return 0;
         }
 
-        if (length == 1) {
+
+        if (i + 1 == length) {
             return 1;
         }
 
-        int num = (s[0] - '0') * 10 + (s[1] - '0');
-
-        if (num > 26) { // 大于26，只能解码一个字符
-            return numDecodings(s.substr(1));
-        } else {
-            return numDecodings(s.substr(1)) + numDecodings(s.substr(2));
+        m[i] = numDecodings(s, i + 1, m);
+        if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')) {
+            m[i] += numDecodings(s, i + 2, m);
         }
+
+        return m[i];
     }
 };
 ```
--- 091-解码方法/numDecodings_recursion.cpp
+++ 091-解码方法/numDecodings_recursion.cpp
@@ -1,29 +1,41 @@
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 class Solution {
 public:
     int numDecodings(string s) {
-        int length = s.size();
-
-        if (length == 0) {
-            return 1;
-        } 
-        // 第一个字符为'0'，不能解码
-        if (s[0] == '0') { 
+        if (s.empty()) {
             return 0;
         }
-        
-        if (length == 1) {
-            return 1;
-        } 
+        unordered_map<int, int> m;
 
-        int num = (s[0] - '0') * 10 + (s[1] - '0');
-
-        if (num > 26) { // 大于26，只能解码一个字符
-            return numDecodings(s.substr(1)); 
-        } else {
-            return numDecodings(s.substr(1)) + numDecodings(s.substr(2));
+        return numDecodings(s, 0, m);
+    }
+private:
+    int numDecodings(string& s, int i, unordered_map<int, int>& m) {
+        if (m.count(i)) {
+            return m[i];
         }
+
+        int length = s.size();
+        if (i == length) {
+            return 1;
+        }
+
+        if (s[i] == '0') {
+            return 0;
+        }
+
+        if (i + 1 == length) {
+            return 1;
+        }
+
+        m[i] = numDecodings(s, i + 1, m);
+        if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')) {
+            m[i] += numDecodings(s, i + 2, m);
+        }
+
+        return m[i];
     }
 };
